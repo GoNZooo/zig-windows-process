52a533be4c7488409f7e96a18a6de03298f8fa06
Rickard Andersson <gonz@severnatazvezda.com>
2020-06-21 22:54:49 +0300

    remove inclusion of `win32` lib

--- build.zig
+++ build.zig
@@ -13,9 +13,8 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("zig-gnu-starter", "src/main.zig");
+    const exe = b.addExecutable("find-processes", "src/main.zig");
     exe.linkLibC();
-    exe.addPackagePath("win32", "./dependencies/zig-win32/src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
